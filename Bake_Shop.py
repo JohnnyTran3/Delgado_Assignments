af90724b1adddacf4efdc407cf3e9c146688a76e
JohnnyTran3 <johnnytran2005.jt@gmail.com>
2025-10-30 20:30:46 -0500

    Add files via upload

--- Bake_Shop.py
+++ Bake_Shop.py
@@ -5,6 +5,7 @@ MENU_COST = [0, 5.95, 4.95, 3.95, 3.75]
 SALES_TAX = 0.0945
 SHUTDOWN = False
 total_cost = 0
+usr_input = None
 
 print(f"Boudreaux & Thibodeaux's Bakery \n--------------------------------------")
 
@@ -13,28 +14,27 @@ print(f"1. Muffin: {MENU_COST[1]} \n1. King Cake Slice: {MENU_COST[2]} \n2. Croi
 
 
 while SHUTDOWN != True:
-    usr_input = input("What would you like to order? Please type a whole number from 1 - 4!: ")
-    if usr_input.isalpha():
-        print("Invalid Input, Please input a valid whole number from 1 - 4: ")
-    else:
+    usr_input = input("What would you like to order? Please type a whole number from 1 - 4 or type 999 again to complete!: ")
+    if usr_input.isdigit:
         usr_input = int(usr_input)
-        while usr_input >= 1 or usr_input <= 4:
-            usr_input2 = int(input("How many of that item would you like to order? "))
-            if usr_input2 < 1:
-                print("Cannot order only 0 item, please put a valid whole number greater or equal to 1! ")
-                usr_input2 = int(input("How many of that item would you like to order?"))
-            if usr_input2 > 0:
-                print(f"You have ordered {usr_input2} {MENU[usr_input]} for {MENU_COST[usr_input]}")
-                total_cost += (MENU_COST[usr_input] * usr_input2)
-                print(total_cost)
-            usr_input = int(input("What would you like to order? Please type a whole number from 1 - 4 or type 999 when order is complete!: "))
+        if usr_input == 999:
+            SHUTDOWN = True
+        else:
+            if usr_input < 1 or usr_input > 4:
+                print("Invalid Input, Please input a valid whole number from 1 - 4 or type 999 to complete order!: ")
+            else:
+                usr_input2 = int(input("How many of that item would you like to order? "))
+                if usr_input2 < 1:
+                    print("Cannot order only 0 item, please put a valid whole number greater or equal to 1! ")
+                    usr_input2 = int(input("How many of that item would you like to order?"))
+                if usr_input2 > 0:
+                    print(f"You have ordered {usr_input2} {MENU[usr_input]} for {MENU_COST[usr_input]}")
+                    total_cost += (MENU_COST[usr_input] * usr_input2)
+                    print(total_cost)
+                usr_input = int(input("What would you like to order? Please type a whole number from 1 - 4 or type 999 when order is complete!: "))
             
-    if usr_input == 999:
-        SHUTDOWN = True
     else:
-        while usr_input < 1 or usr_input > 4:
-            print("Invalid Input, Please input a valid whole number from 1 - 4 or type 999 to complete order!: ")
-
+        print("Invalid Input, Please input a valid whole number from 1 - 4: ")
 
 print(f"Your Total is {total_cost}")
             
